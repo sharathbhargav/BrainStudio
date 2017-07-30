654afe353617e4d359018eb285a5232dbdbcc78d
SharathBhargav <shrthsbhargav96@gmail.com>
2017-07-30 11:28:31 +0530

    Initial commit

---
+++ app/src/test/java/brainstudio/s4pl/com/brainstudio/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package brainstudio.s4pl.com.brainstudio;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
